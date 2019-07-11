1bd3747bdfa73e8d83f1cad3c18cdb341e8f8eb7
stephengallo1977 <stephengallo1977@gmail.com>
2019-07-11 17:25:17 -0400

    cleanedCode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// Make array of cars
+// Make an array of cars
 var cars = ["Bentley", "Maserati", "Mercedes Benz", "BMW", "Porsche"];
 
 function displayCarGif() {
@@ -10,7 +10,7 @@ function displayCarGif() {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
 
     console.log(response);
 
@@ -24,83 +24,99 @@ function displayCarGif() {
       // Store the rating data
       var rating = response.data[i].rating;
 
-      // Creating an element to have the rating displayed
+      // Make an element to have the rating displayed
       var pOne = $("<p>").text("Rating: " + rating);
 
-      // Displaying the rating
+      // Display the rating
       carDiv.append(pOne);
 
       // Retrieve the URL for the image
       var imgURL = response.data[i].images.fixed_height_still.url;
 
-      // Creating an element to hold the image
-      var image = $("<img>").attr({"src":imgURL, "data-still":imgURL, "data-animate":response.data[i].images.fixed_height.url, "data-state":"still", "class":"animateImg"});
+      // Make an element to hold the image
+      var image = $("<img>").attr({
+        "src": imgURL,
+        "data-still": imgURL,
+        "data-animate": response.data[i].images.fixed_height.url,
+        "data-state": "still",
+        "class": "animateImg"
+      });
 
-      // Appending the image
+      // Append the image
       carDiv.append(image);
 
-      // Putting the entire movie above the previous movies
+      // Prepend the gifs
       $("#gifs-view").prepend(carDiv);
 
     }
   });
 }
 
-// Function handling when a Gif is clicked
-$(document).on("click", ".animateImg", function() {
+// Make a function that handles when a Gif is clicked
+$(document).on("click", ".animateImg", function () {
 
-      var state = $(this).attr("data-state");
+  var state = $(this).attr("data-state");
 
-      // Logic that updates the state of the Gif that is clicked
-      if (state === "still") {
+  // Logic that updates the state of the Gif that is clicked
+  if (state === "still") {
 
-        $(this).attr({"src":$(this).attr("data-animate"), "data-state":"animate"});
-      }
-      else {
-
-        $(this).attr({"src":$(this).attr("data-still"), "data-state":"still"});
-      }
+    $(this).attr({
+      "src": $(this).attr("data-animate"),
+      "data-state": "animate"
     });
+  } else {
 
-// Function for displaying car data
+    $(this).attr({
+      "src": $(this).attr("data-still"),
+      "data-state": "still"
+    });
+  }
+});
+
+// Make a function for displaying the car data
 function renderButtons() {
 
-    // Deleting the car buttons prior to adding new animal buttons
-    $("#cars-view").empty();
+  // Delete the car buttons prior to adding new car buttons
+  $("#cars-view").empty();
 
-    // Looping through the array of animals
-    for (var i = 0; i < cars.length; i++) {
+  // Loop through the array of cars
+  for (var i = 0; i < cars.length; i++) {
 
-      // Then dynamicaly generating buttons for each car in the array.
-      var a = $("<button>");
-      // Adding a class
-      a.addClass("car-btn");
-      // Adding a data-attribute with a value of the car at index i
-      a.attr("data-name", cars[i]);
-      // Providing the button's text with a value of the movie at index i
-      a.text(cars[i]);
-      // Adding the button to the HTML
-      $("#cars-view").append(a);
-    }
+    // Dynamicaly generate buttons for each car in the array.
+    var a = $("<button>");
+
+    // Add a class
+    a.addClass("car-btn");
+
+    // Add a data-attribute with a value of the car at index i
+    a.attr("data-name", cars[i]);
+
+    // Provide the button's text with a value of the movie at index i
+    a.text(cars[i]);
+
+    // Add the button to the HTML
+    $("#cars-view").append(a);
+  }
 }
 
-// This function handles events where one button is clicked
-$("#add-car").on("click", function(event) {
+// Make a  function That handles events when one button is clicked
+$("#add-car").on("click", function (event) {
 
-  // event.preventDefault() prevents the form from trying to submit itself.
+  // Prevent the form from trying to submit itself.
   event.preventDefault();
 
-  // This grabs the text from the input box
+  // Grab the text from the input box
   var car = $("#car-input").val().trim();
+
   // The car from the textbox is then added to our array
   cars.push(car);
 
-  // calling renderButtons which handles the processing of our cars array
+  // Call renderButtons which handles the processing of our cars array
   renderButtons();
 });
 
-// Adding a click event listener to all elements with a class of "car-btn"
+// Add a click event listener to all elements with a class of "car-btn"
 $(document).on("click", ".car-btn", displayCarGif);
 
-// Calling the renderButtons function at least once to display the initial list of animals
+// Call the renderButtons function at least once to display the initial list of animals
 renderButtons();
